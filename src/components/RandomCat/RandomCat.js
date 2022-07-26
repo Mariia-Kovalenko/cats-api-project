306bd823af575debe946f6f4621f1654dae4b7c3
Mariia-Kovalenko <mariya.kovalenko.2000@gmail.com>
2022-07-26 20:50:29 +0300

    rewrote random cat component into func component

--- src/components/RandomCat/RandomCat.js
+++ src/components/RandomCat/RandomCat.js
@@ -1,102 +1,88 @@
 import CatService from "../../services/CatService";
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import Loader from '../loader/loader';
 import ErrorMessage from '../ErrorMessage/ErrorMessage'
 import {BASE_URL, IMAGES} from '../../services/_constants'
 
-class RandomCat extends Component {
+const RandomCat = (props) => {
 
-    constructor(props) {
-        super(props)
-    }
-    state = {
-        cat: {},
-        loading: true,
-        error: false
-    }
+    const [cat, setCat] = useState({});
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
 
-    catService = new CatService();
+    const catService = new CatService();
 
-    componentDidMount() {
-        this.loadCat();
-    }
+    useEffect(() => {
+        loadCat()
+    }, [])
 
-    onCatLoading = () => {
-        this.setState({
-            loading: true
-        })
+    // console.log(cat);
+
+    const onCatLoading = () => {
+        setLoading(true);
     }
 
-    onError = () => {
-        this.setState({
-            loading: false,
-            error: true
-        })
+    const onError = () => {
+        setError(true);
+        setLoading(false);
     }
 
-    onCatLoaded = (cat) => {
-        // console.log(cat);
-        this.setState({cat: cat[0], loading: false})
+    const onCatLoaded = (cat) => {
+        setCat(cat[0]);
+        setLoading(false);
     }
 
-    loadCat = () => {
-        this.onCatLoading();
-        this.catService
-            .loadData(BASE_URL + IMAGES, {limit: 1, order: 'Desc', page: this.getRandomPage()})
-                .then(this.onCatLoaded)
-                .catch(this.onError)
+    const loadCat = () => {
+        onCatLoading();
+        catService.loadData(BASE_URL + IMAGES, {limit: 1, order: 'Desc', page: getRandomPage()})
+                .then(onCatLoaded)
+                .catch(onError)
     }
 
-    getRandomPage = () => {
+    const getRandomPage = () => {
         return Math.floor(Math.random() * 3000);
     }
 
-    addFavourites = (cat, mark) => {
-        this.props.addFavourites(cat, mark);
-        this.loadCat();
+    const addFavourites = (cat, mark) => {
+        props.addFavourites(cat, mark);
+        loadCat();
     }
 
-    render() {
-
-        let {cat, loading, error} = this.state;
-        // console.log(cat);
-        const loader = loading ? <Loader/> : null;
-        
-        let buttonsClasses = "random-cat__buttons "
-        if (loading || error) {
-            buttonsClasses += 'hide'
-        }
-
-        const errorMessage = error ? <ErrorMessage/> : null;
-        const content = !(loading || error) ? <View cat={cat}/> : null;
-
-        return (
-            <div className="random-cat__container">
-                {loader}
-                {content}
-                {errorMessage}
-                <div className={buttonsClasses}>
-                    <button  className="cat__button dislike">
-                        <img src="images/dislike-white.svg" alt='dislike'></img>
-                    </button>
+    const loader = loading ? <Loader/> : null;
     
-                    <button onClick={this.loadCat} className="cat__button like">
-                        <img src="images/heart-white.svg" alt='like'></img>
-                    </button>
-    
-                    <button onClick={() => this.addFavourites(cat, 'fav')} className="cat__button fav">
-                        <img src="images/star-white.svg" alt='fav'></img>
-                    </button>
-                </div>
+    let buttonsClasses = "random-cat__buttons "
+    if (loading || error) {
+        buttonsClasses += 'hide'
+    }
+
+    const errorMessage = error ? <ErrorMessage/> : null;
+    const content = !(loading || error) ? <View cat={cat}/> : null;
+
+    return (
+        <div className="random-cat__container">
+            {loader}
+            {content}
+            {errorMessage}
+            <div className={buttonsClasses}>
+                <button  className="cat__button dislike">
+                    <img src="images/dislike-white.svg" alt='dislike'></img>
+                </button>
+
+                <button onClick={loadCat} className="cat__button like">
+                    <img src="images/heart-white.svg" alt='like'></img>
+                </button>
+
+                <button onClick={() => addFavourites(cat, 'fav')} className="cat__button fav">
+                    <img src="images/star-white.svg" alt='fav'></img>
+                </button>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 const View = ({cat}) => {
 
     let {id, url} = cat;
-    // console.log('cat', id);
 
     return (
         <div className="random-cat__image">
